1e7375eac4f4b82b08531167e48c5b8ce916658d
leandro-costa <leandro.costa@ifba.edu.br>
2024-09-24 19:26:28 -0300

    collections

---
+++ src/posts/code/collections/TestaPerformance.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class TestaPerformance {
+    public static void main(String[] args) {
+        System.out.println("Iniciando...");
+        //Collection<Integer> teste = new ArrayList();
+        Collection<Integer> teste = new HashSet<>();
+        long inicio = System.currentTimeMillis();
+        int total = 300000;
+        for (int i = 0; i < total; i++) {
+            teste.add(i);
+        }
+        for (int i = 0; i < total; i++) {
+            teste.contains(i);
+        }
+        long fim = System.currentTimeMillis();
+        long tempo = fim - inicio;
+        System.out.println("Tempo gasto: " + tempo);
+    }
+}
